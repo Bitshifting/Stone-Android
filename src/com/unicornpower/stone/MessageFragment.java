9143d7369ea864900770c51f86561408cedaaf2a
sschwebach <sschwebach@wisc.edu>
2014-02-08 10:24:23 -0600

    added successful server messages

--- src/com/unicornpower/stone/MessageFragment.java
+++ src/com/unicornpower/stone/MessageFragment.java
@@ -1,5 +1,6 @@
 package com.unicornpower.stone;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.res.Configuration;
 import android.os.Bundle;
@@ -9,6 +10,7 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
@@ -16,9 +18,10 @@ import android.widget.RadioGroup;
 public class MessageFragment extends Fragment{
 
 	private final Context mContext;
+	private EditText mEdit;
 	
 	public MessageFragment(){
-		mContext = this.getActivity();
+		mContext = (Context) this.getActivity();
 	}
 
 	@Override
@@ -32,13 +35,15 @@ public class MessageFragment extends Fragment{
 		rootView = (ViewGroup) inflater.inflate(R.layout.fragment_message, container, false);
 		((ListView) rootView.findViewById(R.id.friend_list)).setAdapter(new FriendListAdapter(this.getActivity()));
 		((RadioButton) ((RadioGroup) rootView.findViewById(R.id.privacy_group)).getChildAt(0)).setChecked(true);
+		mEdit = (EditText) rootView.findViewById(R.id.message_space);
 		((Button) rootView.findViewById(R.id.post_button)).setOnClickListener(new OnClickListener(){
 
 			@Override
 			public void onClick(View v) {
+				String message = mEdit.getText().toString();
 				// TODO Auto-generated method stub
 				ServerAPITask task = new ServerAPITask();
-				task.setAPIRequest("http://riptide.alexkersten.com:3333/stoneapi/get_local_metadata/40.42854/-86.9222/10000000");
+				task.setAPIRequest("http://riptide.alexkersten.com:3333/stoneapi/post_message/" + message.replaceAll(" ", "%20") + "/40.42853/-86.9222/SmartAssSam");
 				task.execute("Hello");
 			}
 			
